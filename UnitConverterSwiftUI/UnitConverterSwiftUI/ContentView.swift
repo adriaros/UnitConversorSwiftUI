3c06a2a6ce45fe09622fae3ccc2e33b1d420a8f4
AdriaRos <adria.ros@elma.care>
2021-03-14 18:56:38 +0100

    main
    - Added a new project

---
+++ UnitConverterSwiftUI/UnitConverterSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UnitConverterSwiftUI
+//
+//  Created by Adrià Ros on 14/3/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
